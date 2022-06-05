45aa6cb1a5cad209fe586a0e3b68092680b4205b
Max Gonzih <gonzih@gmail.com>
2022-06-05 13:14:34 -0700

    Oh wow

--- notebooks/scripts/server.jl
+++ notebooks/scripts/server.jl
@@ -1,3 +1,6 @@
-using PlutoSliderServer
+# using PlutoSliderServer
+# PlutoSliderServer.run_directory("/notebooks"; SliderServer_host="0.0.0.0")
 
-PlutoSliderServer.run_directory("/notebooks"; SliderServer_host="0.0.0.0")
+using Pluto
+
+Pluto.run(host="0.0.0.0", port=8888, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false, dismiss_update_notification=true, show_file_system=true)
