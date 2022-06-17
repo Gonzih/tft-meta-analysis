13d5e50df0fde87f45bd8db39ceca95eb1ccc2c5
Max Gonzih <gonzih@gmail.com>
2022-06-17 14:02:53 -0700

    Oh this is what it is

--- notebooks/scripts/export.jl
+++ notebooks/scripts/export.jl
@@ -8,7 +8,3 @@ println(describe(df.matches))
 
 println("Saving matches")
 @time export_all_data(df)
-
-@time df = import_all_data(10000)
-println(describe(df.matches))
-println("Test loaded matches")
