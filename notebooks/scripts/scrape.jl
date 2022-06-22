eea9b68af4e2257cbd273e8eee31a4f05065b8b4
Max Gonzih <gonzih@gmail.com>
2022-06-21 21:18:05 -0700

    Reduce scope of scraping

--- notebooks/scripts/scrape.jl
+++ notebooks/scripts/scrape.jl
@@ -1,6 +1,6 @@
 include("../src/riot.jl")
 using Main.Riot
 
-leagues = ["grandmaster", "challenger", "master"]
+leagues = ["grandmaster", "challenger"]
 l = length(map(scrape_league, leagues))
 println("Scraped $(l) requests")
