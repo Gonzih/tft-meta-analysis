4c0269c059152cf985aaa468348674c97e6c8790
Max Gonzih <gonzih@gmail.com>
2022-06-24 09:02:21 -0700

    Lets try scraping master again?

--- notebooks/scripts/scrape.jl
+++ notebooks/scripts/scrape.jl
@@ -1,6 +1,6 @@
 include("../src/riot.jl")
 using Main.Riot
 
-leagues = ["grandmaster", "challenger"]
+leagues = ["grandmaster", "challenger", "master"]
 l = length(map(scrape_league, leagues))
 println("Scraped $(l) requests")
