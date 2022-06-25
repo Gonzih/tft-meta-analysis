69c6cecc3ab992dc0286f10cc0babdcc9a083cf8
Max Gonzih <gonzih@gmail.com>
2022-06-24 20:29:31 -0700

    Revert "Speedud up scraping for now"
    This reverts commit 8c05a22644745b2eebd6c7b6d33dbf244bea0a4e.

--- notebooks/scripts/scrape.jl
+++ notebooks/scripts/scrape.jl
@@ -1,7 +1,6 @@
 include("../src/riot.jl")
 using Main.Riot
 
-# leagues = ["grandmaster", "challenger", "master"]
-leagues = ["grandmaster", "challenger"]
+leagues = ["grandmaster", "challenger", "master"]
 l = length(map(scrape_league, leagues))
 println("Scraped $(l) requests")
