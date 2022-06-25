8c05a22644745b2eebd6c7b6d33dbf244bea0a4e
Max Gonzih <gonzih@gmail.com>
2022-06-24 20:29:16 -0700

    Speedud up scraping for now

--- notebooks/scripts/scrape.jl
+++ notebooks/scripts/scrape.jl
@@ -1,6 +1,7 @@
 include("../src/riot.jl")
 using Main.Riot
 
-leagues = ["grandmaster", "challenger", "master"]
+# leagues = ["grandmaster", "challenger", "master"]
+leagues = ["grandmaster", "challenger"]
 l = length(map(scrape_league, leagues))
 println("Scraped $(l) requests")
